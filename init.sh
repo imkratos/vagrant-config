98af566f9e9a7c20e436e21858ca9cbff179f15d
刘志硕 <liuzhishuo@gaosiedu.com>
2019-08-15 10:58:49 +0800

    init

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@
 
 sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
 
-sudo touch /etc/apt/sources.list
+sudo echo "" > /etc/apt/sources.list
 
 echo "
 192.168.59.2 node2
@@ -11,36 +11,36 @@ echo "
 192.168.59.4 node4
 " >> /etc/hosts
 
-# echo "
-# deb http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse
-# deb-src http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse
-
-# deb http://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse
-# deb-src http://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse
-
-# deb http://mirrors.aliyun.com/ubuntu/ bionic-updates main restricted universe multiverse
-# deb-src http://mirrors.aliyun.com/ubuntu/ bionic-updates main restricted universe multiverse
-
-# deb http://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe multiverse
-# deb-src http://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe multiverse
-
-# deb http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse
-# deb-src http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse
-
-# " >> /etc/apt/sources.list
-
 echo "
-deb http://mirrors.163.com/ubuntu/ bionic main restricted universe multiverse
-deb http://mirrors.163.com/ubuntu/ bionic-security main restricted universe multiverse
-deb http://mirrors.163.com/ubuntu/ bionic-updates main restricted universe multiverse
-deb http://mirrors.163.com/ubuntu/ bionic-proposed main restricted universe multiverse
-deb http://mirrors.163.com/ubuntu/ bionic-backports main restricted universe multiverse
-deb-src http://mirrors.163.com/ubuntu/ bionic main restricted universe multiverse
-deb-src http://mirrors.163.com/ubuntu/ bionic-security main restricted universe multiverse
-deb-src http://mirrors.163.com/ubuntu/ bionic-updates main restricted universe multiverse
-deb-src http://mirrors.163.com/ubuntu/ bionic-proposed main restricted universe multiverse
-deb-src http://mirrors.163.com/ubuntu/ bionic-backports main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse
+
+deb http://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse
+
+deb http://mirrors.aliyun.com/ubuntu/ bionic-updates main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-updates main restricted universe multiverse
+
+deb http://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe multiverse
+
+deb http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse
+
 " >> /etc/apt/sources.list
+
+# echo "
+# deb http://mirrors.163.com/ubuntu/ bionic main restricted universe multiverse
+# deb http://mirrors.163.com/ubuntu/ bionic-security main restricted universe multiverse
+# deb http://mirrors.163.com/ubuntu/ bionic-updates main restricted universe multiverse
+# deb http://mirrors.163.com/ubuntu/ bionic-proposed main restricted universe multiverse
+# deb http://mirrors.163.com/ubuntu/ bionic-backports main restricted universe multiverse
+# deb-src http://mirrors.163.com/ubuntu/ bionic main restricted universe multiverse
+# deb-src http://mirrors.163.com/ubuntu/ bionic-security main restricted universe multiverse
+# deb-src http://mirrors.163.com/ubuntu/ bionic-updates main restricted universe multiverse
+# deb-src http://mirrors.163.com/ubuntu/ bionic-proposed main restricted universe multiverse
+# deb-src http://mirrors.163.com/ubuntu/ bionic-backports main restricted universe multiverse
+# " >> /etc/apt/sources.list
 
 
 sudo apt-get update
